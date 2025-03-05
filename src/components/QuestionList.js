b8cbfd3cfcb68b3da64292fb109e4d256d21f90f
darlenenyambura <darlene.nyambura@student.moringaschool.com>
2025-03-05 08:59:52 +0300

    passed delete function from list to item

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -9,6 +9,12 @@ function QuestionList() {
     .then((data) => setQuestions(data))
       .catch((error) => console.error("Error fetching questions", error));
     }, []);
+    function handleDeleteQuestion(id) {
+      fetch (`http://localhost:4000/questions/${id}`, {
+        method: "DELETE",
+      })
+      
+    }
   return (
     <section>
       <h1>Quiz Questions</h1>
